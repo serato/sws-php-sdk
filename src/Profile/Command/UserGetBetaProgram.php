8b9ef20ab1942dbc426c5f6fe4246e040df7277a
Gabriel <youngmin.jo@serato.com>
2019-07-30 15:31:33 +1200

    added User add & get beta program

---
+++ src/Profile/Command/UserGetBetaProgram.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Serato\SwsSdk\Profile\Command;
+
+use Serato\SwsSdk\CommandBasicAuth;
+
+/**
+ * Gets beta program list for an authenticated user.
+ *
+ * Valid keys for the `$args` array provided to the constructor are:
+ *
+ * - `user_id`: (integer) Required. User ID.
+ *
+ * This command can be executed on a `Serato\SwsSdk\Profile\ProfileClient` instance
+ * using the `ProfileClient::getUserBetaProgram` magic method.
+ */
+class UserGetBetaProgram extends CommandBasicAuth
+{
+     /**
+     * {@inheritdoc}
+     */
+    public function getBody(): string
+    {
+        $args = $this->commandArgs;
+        unset($args['user_id']);
+        return $this->arrayToFormUrlEncodedBody($args);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHttpMethod(): string
+    {
+        return 'GET';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUriPath(): string
+    {
+        return '/users/' . $this->commandArgs['user_id'] . '/betaprograms';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUriQuery(): string
+    {
+        return http_build_query($this->commandArgs);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getArgsDefinition(): array
+    {
+        return [
+            'user_id' => ['type' => self::ARG_TYPE_INTEGER, 'required' => true]
+        ];
+    }
+}
