f6be095e92652ec157c136b7d72a7e430669b149
MikeS <mike.sabharwal@serato.com>
2019-12-24 10:01:48 +1300

    [WEB-3761] fix namespace for tests

--- tests/Da/DaClientTest.php
+++ tests/Da/DaClientTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Serato\SwsSdk\Test\Ecom;
+namespace Serato\SwsSdk\Test\Da;
 
 use Serato\SwsSdk\Da\DaClient;
 use Serato\SwsSdk\Test\AbstractTestCase;
--- tests/Notifications/NotificationsClientTest.php
+++ tests/Notifications/NotificationsClientTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Serato\SwsSdk\Test\Ecom;
+namespace Serato\SwsSdk\Test\Notifications;
 
 use Serato\SwsSdk\Da\DaClient;
 use Serato\SwsSdk\Test\AbstractTestCase;
