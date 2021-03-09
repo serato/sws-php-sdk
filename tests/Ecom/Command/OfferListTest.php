8dedba26b883f44aa68e8d3322ff34fb65f3cfe1
Gabriel <youngmin.jo@serato.com>
2021-03-10 11:23:20 +1300

    fix parameter reqired to optional

--- tests/Ecom/Command/OfferListTest.php
+++ tests/Ecom/Command/OfferListTest.php
@@ -9,36 +9,6 @@ use Serato\SwsSdk\Ecom\Command\OfferList;
 class OfferListTest extends AbstractTestCase
 {
     /**
-     * @return array<array>
-     */
-    public function missingRequiredArgProvider(): array
-    {
-        return [
-            [[]],
-            [['foo' => 'parameter']]
-        ];
-    }
-
-     /**
-     * Tests that an exception is thrown if required parameters are missing.
-     *
-     * @param array<string, DateTime|int|string> $args
-     *
-     * @dataProvider missingRequiredArgProvider
-     * @expectedException \InvalidArgumentException
-     */
-    public function testMissingRequiredArg(array $args): void
-    {
-        $command = new OfferList(
-            'app_id',
-            'app_password',
-            'http://my.server.com',
-            $args
-        );
-        $command->getRequest();
-    }
-
-    /**
      * Smoke test to verify that the command is creating a valid request.
      *
      * @return void
