1b71b0c80bf4b6cbeaeef8d61f2ffbdda4aef88e
Gabriel <youngmin.jo@serato.com>
2021-03-11 16:52:51 +1300

    Change command method name and class

--- src/Ecom/EcomClient.php
+++ src/Ecom/EcomClient.php
@@ -38,7 +38,7 @@ class EcomClient extends Client
             'CancelSubscription'       => '\\Serato\\SwsSdk\\Ecom\\Command\\SubscriptionCancel',
             'CreateInvoice'            => '\\Serato\\SwsSdk\\Ecom\\Command\\InvoiceCreate',
             'UpdateCartBillingAddress' => '\\Serato\\SwsSdk\\Ecom\\Command\\UpdateCartBillingAddress',
-            'GetOffers'                => '\\Serato\\SwsSdk\\Ecom\\Command\\OfferList',
+            'GetVoucherTypes'                => '\\Serato\\SwsSdk\\Ecom\\Command\\VoucherTypeList',
             'CreateVoucher'            => '\\Serato\\SwsSdk\\Ecom\\Command\\VoucherCreate',
             'AssignVoucher'            => '\\Serato\\SwsSdk\\Ecom\\Command\\VoucherAssign'
         ];
